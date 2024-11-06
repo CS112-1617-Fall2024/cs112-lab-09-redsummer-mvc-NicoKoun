d8aa271a3dab2a8a191e1ef715520e9c5c29ffa4
MCCLab4804WD08 <nico.koundakjian@gmail.com>
2024-11-05 19:19:42 -0800

    started lab

--- src/main/java/cs112/lab09/controllers/MainController.java
+++ src/main/java/cs112/lab09/controllers/MainController.java
@@ -1,14 +1,22 @@
 package cs112.lab09.controllers;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 public class MainController {
     @FXML
-    private Label welcomeText;
+    private ImageView logoImageView;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Button startButton;
+
+    @FXML
+    public void initialize(){
+        logoImageView.setImage(new Image(getClass().getResourceAsStream("/images/Red-Summer.jpg")));
     }
+    @FXML
+    protected void onStartButtonClicked(){}
 }
